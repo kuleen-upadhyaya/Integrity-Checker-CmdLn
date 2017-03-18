9aa79b7d3b8379e4fb4d0b47b36c05160c7ca571
kuleen-upadhyaya <kuleen.upadhyaya>
2017-03-18 10:57:53 +0530

    added common methods

--- src/main/java/cool/ic/utils/Commons.java
+++ src/main/java/cool/ic/utils/Commons.java
@@ -13,4 +13,32 @@ public class Commons
 	{
 		logger.error(message, t);
 	}
+	
+	public static long toMB(long fileSize) 
+	{
+		return fileSize/(1024*1024);
+	}
+
+	public static String getTimeString(long startTime, long endTime) 
+	{
+		StringBuilder sbrTime = new StringBuilder();
+		
+		long timeTakenSec = (endTime - startTime)/1000000000L;
+		
+		int hours = (int) timeTakenSec / 3600;
+	    int remainder = (int) timeTakenSec - hours * 3600;
+	    int mins = remainder / 60;
+	    remainder = remainder - mins * 60;
+	    int secs = remainder;
+
+	    sbrTime.append("Total time taken : ");
+		sbrTime.append(hours);
+		sbrTime.append(" Hr ");
+		sbrTime.append(mins);
+		sbrTime.append(" Min ");
+		sbrTime.append(secs);
+		sbrTime.append(" Sec");
+		
+	    return sbrTime.toString();
+	}
 }
